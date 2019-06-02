372805215afa1c7f8f9b7978736f0b463043f709
Bill Wang <bill.wang>
2019-06-02 17:49:27 +1000

    fix the fetch issue and refactor the codes with eslint

--- index.js
+++ index.js
@@ -1,150 +1,153 @@
 #!/usr/bin/env node
 
-var packageJson = require("./package.json");
-var fs = require('fs');
+const fs = require('fs');
 
-var AWS = require("aws-sdk");
-var AWSCognito = require("amazon-cognito-identity-js");
-var apigClientFactory = require("aws-api-gateway-client").default;
-var WindowMock = require("window-mock").default;
+global.fetch = require('node-fetch');
+
+const AWS = require('aws-sdk');
+const AWSCognito = require('amazon-cognito-identity-js');
+const apigClientFactory = require('aws-api-gateway-client').default;
+const WindowMock = require('window-mock').default;
 
 global.window = { localStorage: new WindowMock().localStorage };
-global.navigator = function() {
+global.navigator = function () {
   return null;
 };
 
-var argv = require("yargs")
-  .option("username", {
-    describe: "Username of the user",
-    demandOption: true
+const { argv } = require('yargs')
+  .option('username', {
+    describe: 'Username of the user',
+    demandOption: true,
   })
-  .option("password", {
-    describe: "Password of the user",
-    demandOption: true
+  .option('password', {
+    describe: 'Password of the user',
+    demandOption: true,
   })
-  .option("user-pool-id", {
-    describe: "Cognito user pool id",
-    demandOption: true
+  .option('user-pool-id', {
+    describe: 'Cognito user pool id',
+    demandOption: true,
   })
-  .option("app-client-id", {
-    describe: "Cognito user pool app client id",
-    demandOption: true
+  .option('app-client-id', {
+    describe: 'Cognito user pool app client id',
+    demandOption: true,
   })
-  .option("cognito-region", {
-    describe: "Cognito region",
-    default: "us-east-1"
+  .option('cognito-region', {
+    describe: 'Cognito region',
+    default: 'us-east-1',
   })
-  .option("identity-pool-id", {
-    describe: "Cognito identity pool id",
-    demandOption: true
+  .option('identity-pool-id', {
+    describe: 'Cognito identity pool id',
+    demandOption: true,
   })
-  .option("invoke-url", {
-    describe: "API Gateway URL",
-    demandOption: true
+  .option('invoke-url', {
+    describe: 'API Gateway URL',
+    demandOption: true,
   })
-  .option("api-gateway-region", {
-    describe: "API Gateway region",
-    default: "us-east-1"
+  .option('api-gateway-region', {
+    describe: 'API Gateway region',
+    default: 'us-east-1',
   })
-  .option("api-key",{
-    describe: "API Key",
-    default: undefined
+  .option('api-key', {
+    describe: 'API Key',
+    default: undefined,
   })
-  .option("path-template", {
-    describe: "API path template",
-    demandOption: true
+  .option('path-template', {
+    describe: 'API path template',
+    demandOption: true,
   })
-  .option("method", {
-    describe: "API method",
-    default: "GET"
+  .option('method', {
+    describe: 'API method',
+    default: 'GET',
   })
-  .option("params", {
-    describe: "API request params",
-    default: "{}"
+  .option('params', {
+    describe: 'API request params',
+    default: '{}',
   })
-  .option("additional-params", {
-    describe: "API request additional params",
-    default: "{}"
+  .option('additional-params', {
+    describe: 'API request additional params',
+    default: '{}',
   })
-  .option("body", {
-    describe: "API request body",
-    default: "{}"
+  .option('body', {
+    describe: 'API request body',
+    default: '{}',
   })
-  .option("access-token-header", {
-    describe: "Header to use to pass access token with request"
+  .option('access-token-header', {
+    describe: 'Header to use to pass access token with request',
   })
-  .help("h")
-  .alias("h", "help")
-  .alias("v", "version")
+  .help('h')
+  .alias('h', 'help')
+  .alias('v', 'version')
   .version(packageJson.version)
-  .wrap(null).argv;
+  .wrap(null);
+const packageJson = require('./package.json');
 
 function authenticate(callback) {
-  var poolData = {
+  const poolData = {
     UserPoolId: argv.userPoolId,
-    ClientId: argv.appClientId
+    ClientId: argv.appClientId,
   };
 
   AWS.config.update({ region: argv.cognitoRegion });
-  var userPool = new AWSCognito.CognitoUserPool(poolData);
+  const userPool = new AWSCognito.CognitoUserPool(poolData);
 
-  var userData = {
+  const userData = {
     Username: argv.username,
-    Pool: userPool
+    Pool: userPool,
   };
-  var authenticationData = {
+  const authenticationData = {
     Username: argv.username,
-    Password: argv.password
+    Password: argv.password,
   };
-  var authenticationDetails = new AWSCognito.AuthenticationDetails(
-    authenticationData
+  const authenticationDetails = new AWSCognito.AuthenticationDetails(
+    authenticationData,
   );
 
-  var cognitoUser = new AWSCognito.CognitoUser(userData);
+  const cognitoUser = new AWSCognito.CognitoUser(userData);
 
-  console.log("Authenticating with User Pool");
+  console.log('Authenticating with User Pool');
 
   cognitoUser.authenticateUser(authenticationDetails, {
-    onSuccess: function(result) {
+    onSuccess(result) {
       callback({
         idToken: result.getIdToken().getJwtToken(),
-        accessToken: result.getAccessToken().getJwtToken()
+        accessToken: result.getAccessToken().getJwtToken(),
       });
     },
-    onFailure: function(err) {
+    onFailure(err) {
       console.log(err.message ? err.message : err);
     },
-    newPasswordRequired: function() {
-      console.log("Given user needs to set a new password");
+    newPasswordRequired() {
+      console.log('Given user needs to set a new password');
     },
-    mfaRequired: function() {
-      console.log("MFA is not currently supported");
+    mfaRequired() {
+      console.log('MFA is not currently supported');
     },
-    customChallenge: function() {
-      console.log("Custom challenge is not currently supported");
-    }
+    customChallenge() {
+      console.log('Custom challenge is not currently supported');
+    },
   });
 }
 
 function getCredentials(userTokens, callback) {
-  console.log("Getting temporary credentials");
+  console.log('Getting temporary credentials');
 
-  var logins = {};
-  var idToken = userTokens.idToken;
-  var accessToken = userTokens.accessToken;
+  const logins = {};
+  const { idToken } = userTokens;
+  const { accessToken } = userTokens;
 
   logins[
-    "cognito-idp." + argv.cognitoRegion + ".amazonaws.com/" + argv.userPoolId
+    `cognito-idp.${argv.cognitoRegion}.amazonaws.com/${argv.userPoolId}`
   ] = idToken;
 
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: argv.identityPoolId,
-    Logins: logins
+    Logins: logins,
   });
 
-  AWS.config.credentials.get(function(err) {
+  AWS.config.credentials.get((err) => {
     if (err) {
       console.log(err.message ? err.message : err);
+
       return;
     }
 
@@ -153,51 +156,54 @@ function getCredentials(userTokens, callback) {
 }
 
 function makeRequest(userTokens) {
-  console.log("Making API request");
+  console.log('Making API request');
 
-  var apigClient = apigClientFactory.newClient({
+  const apigClient = apigClientFactory.newClient({
     apiKey: argv.apiKey,
     accessKey: AWS.config.credentials.accessKeyId,
     secretKey: AWS.config.credentials.secretAccessKey,
     sessionToken: AWS.config.credentials.sessionToken,
     region: argv.apiGatewayRegion,
-    invokeUrl: argv.invokeUrl
+    invokeUrl: argv.invokeUrl,
   });
 
-  var params = JSON.parse(argv.params);
-  var additionalParams = JSON.parse(argv.additionalParams);
+  const params = JSON.parse(argv.params);
+  const additionalParams = JSON.parse(argv.additionalParams);
 
-  var body = '';
-  if (argv.body.startsWith("@")) {
-     // body from file
-     const bodyFromFile = argv.body.replace(/^@/, "");
-     const contentFromFile = fs.readFileSync(bodyFromFile);
-     body = JSON.parse(contentFromFile);
+  let body = '';
+
+  if (argv.body.startsWith('@')) {
+    // body from file
+    const bodyFromFile = argv.body.replace(/^@/, '');
+    const contentFromFile = fs.readFileSync(bodyFromFile);
+
+    body = JSON.parse(contentFromFile);
   } else {
-     body = JSON.parse(argv.body);
-  };
+    body = JSON.parse(argv.body);
+  }
 
   if (argv.accessTokenHeader) {
     const tokenHeader = {};
+
     tokenHeader[argv.accessTokenHeader] = userTokens.accessToken;
     additionalParams.headers = Object.assign({}, additionalParams.headers, tokenHeader);
   }
 
   apigClient
     .invokeApi(params, argv.pathTemplate, argv.method, additionalParams, body)
-    .then(function(result) {
+    .then((result) => {
       console.dir({
         status: result.status,
         statusText: result.statusText,
-        data: result.data
+        data: result.data,
       });
     })
-    .catch(function(result) {
+    .catch((result) => {
       if (result.response) {
         console.dir({
           status: result.response.status,
           statusText: result.response.statusText,
-          data: result.response.data
+          data: result.response.data,
         });
       } else {
         console.log(result.message);
@@ -205,6 +211,6 @@ function makeRequest(userTokens) {
     });
 }
 
-authenticate(function(tokens) {
+authenticate((tokens) => {
   getCredentials(tokens, makeRequest);
 });
